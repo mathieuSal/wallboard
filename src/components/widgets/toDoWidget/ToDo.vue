59e3e131cd982453d0bde18c8738901d985f1893
mathieu salles <mathieusalles@MacBook-Pro-de-mathieu.local>
2020-05-27 18:53:31 +0200

    feat: routing for todo widget

---
+++ src/components/widgets/toDoWidget/ToDo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="back-home">
+      <router-link to="/">
+        <p>home</p>
+      </router-link>
+    </div>
+    <div id="todo-widget">
+      <p>here we will have our todo list component</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ToDo',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.back-home {
+  position: absolute;
+  top: 1rem;
+  left: 1rem;
+}
+</style>
