9de7e5226a9c5b760be8ed4686e3f25bb646f162
mathieu salles <mathieusalles@MacBook-Pro.local>
2020-06-10 21:23:15 +0200

    feat(bookmark): routing and widget creation

---
+++ front/src/components/widgets/bookmarkWidget/Bookmark.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bookmarkBody">
+    <home-component></home-component>
+  </div>
+</template>
+
+<script>
+import HomeComponent from '../../generic/homeComponent/HomeComponent'
+
+export default {
+  name: 'Bookmark',
+  components: {
+    HomeComponent
+  },
+  data: function () {
+    return {}
+  }
+}
+</script>
