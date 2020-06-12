db064933af9eff17f03eb17ad97390469636a1ec
mathieu salles <mathieusalles@MacBook-Pro.local>
2020-06-12 11:43:12 +0200

    fix(utils): eslint fix

--- front/src/utils/wallboardUtils.js
+++ front/src/utils/wallboardUtils.js
@@ -5,7 +5,7 @@
 export const hashCode = s => s.split('').reduce((a, b) => { a = ((a << 5) - a) + b.charCodeAt(0); return a & a }, 0)
 
 export const hex2rgb = hex => {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
   return result ? {
     r: parseInt(result[1], 16),
     g: parseInt(result[2], 16),
